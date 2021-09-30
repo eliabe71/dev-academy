f333bd339ed1ffda4d01e904dfbc2486d8c0741f
Eliabe <eliabesantos1106@gmail.com>
2021-09-29 23:57:28 -0300

    proxymodel

--- proxymodel.cpp
+++ proxymodel.cpp
@@ -1,7 +1,28 @@
 #include "proxymodel.h"
 #include <QSortFilterProxyModel>
 
-proxyModel::proxyModel() : public QSortFilterProxyModel
+proxyModel::proxyModel(QObject *parent) : QSortFilterProxyModel(parent)
 {
 
+}
+
+QVariant proxyModel::filterUser()
+{
+    return this->id_user;
+}
+
+void proxyModel::setFilterUser(int id)
+{
+    this->id_user = id;
+}
+
+bool proxyModel::filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const
+{
+    QModelIndex index =  sourceModel()->index(sourceRow,5, sourceParent);
+    return (sourceModel()->data(index).toInt() == id_user);
+}
+
+QVariant proxyModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    return sourceModel()->headerData(section, orientation,role);
 }
--- proxymodel.h
+++ proxymodel.h
@@ -2,13 +2,19 @@
 #define PROXYMODEL_H
 #include <QSortFilterProxyModel>
 #include <QObject>
-
+#include <QVariant>
 class proxyModel : public QSortFilterProxyModel
 {
     Q_OBJECT
 public:
-    explict proxyModel();
-
+     proxyModel(QObject *parent = 0);
+     QVariant filterUser();
+     void setFilterUser(int id);
+protected:
+    bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const override;
+    QVariant headerData(int section, Qt::Orientation orientation,int role) const override;
+private :
+    int id_user;
 };
 
 #endif // PROXYMODEL_H
